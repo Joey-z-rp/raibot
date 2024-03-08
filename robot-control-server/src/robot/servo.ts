f13170aea3a2ae241f9d2976639a363207ac3c04
Joey Zheng <joey.zheng@smokeball.com>
2024-03-08 13:55:29 +1000

    add speed control

--- robot-control-server/src/robot/servo.ts
+++ robot-control-server/src/robot/servo.ts
@@ -4,6 +4,9 @@ import {
   positionToMovementDirectionMap,
 } from "./constants";
 
+const MOVE_INTERVAL = 20; // 20ms
+const BASE_MOVE_AMOUNT = 1; // 1 degree
+
 const rotateServo =
   process.platform === "darwin"
     ? (position: string, angle: number) =>
@@ -18,9 +21,17 @@ type OperationLimit = {
 export class Servo {
   private angle: number;
 
+  private targetAngle: number;
+
   private operationLimit: OperationLimit;
 
   private position: Position;
+
+  private speed: number;
+
+  private isMoving: boolean;
+
+  private movingTimer: NodeJS.Timeout;
 
   constructor({
     operationLimit,
@@ -30,24 +41,61 @@ export class Servo {
     position: Position;
   }) {
     this.angle = 90;
+    this.targetAngle = 90;
     this.operationLimit = operationLimit;
     this.position = position;
+    this.speed = 5;
+    this.isMoving = false;
   }
 
   turnToAngle() {
-    rotateServo(positionToChannelMap[this.position], this.angle);
+    this.isMoving = true;
+
+    return new Promise((res) => {
+      const turn = () => {
+        const movingAmount = BASE_MOVE_AMOUNT * this.speed;
+        const movingDirection = this.targetAngle - this.angle > 0 ? 1 : -1;
+        const resultingAngle = this.angle + movingAmount * movingDirection;
+        const isDone =
+          (movingDirection === 1 && resultingAngle >= this.targetAngle) ||
+          (movingDirection === -1 && resultingAngle <= this.targetAngle);
+        const targetAngleForThisInterval = isDone
+          ? this.targetAngle
+          : resultingAngle;
+        rotateServo(
+          positionToChannelMap[this.position],
+          targetAngleForThisInterval
+        );
+
+        if (!isDone) {
+          this.movingTimer = setTimeout(turn, MOVE_INTERVAL);
+        } else {
+          this.isMoving = false;
+          res(undefined);
+        }
+      };
+
+      turn();
+    });
   }
 
-  setAngle(angle: number) {
+  stop() {
+    if (this.movingTimer) clearTimeout(this.movingTimer);
+    this.isMoving = false;
+  }
+
+  setTargetAngle(angle: number) {
     if (angle < this.operationLimit.low || angle > this.operationLimit.high) {
       return console.warn(`Angle ${angle} exceeds operation limit`);
     }
-    this.angle = angle;
+    this.targetAngle = angle;
     this.turnToAngle();
   }
 
   move(amount: number) {
-    this.setAngle(
+    if (this.isMoving) return;
+
+    this.setTargetAngle(
       this.angle + amount * positionToMovementDirectionMap[this.position]
     );
   }
