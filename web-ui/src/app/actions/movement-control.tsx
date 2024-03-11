7558303d5df248696ad9bf50de1a5df8a5eaab40
Joey Zheng <joey.zheng@smokeball.com>
2024-03-11 20:03:45 +1000

    fix continuous actions

--- web-ui/src/app/actions/movement-control.tsx
+++ web-ui/src/app/actions/movement-control.tsx
@@ -7,18 +7,7 @@ export const MovementControl = ({
 }: {
   sendCommand: SendCommand;
 }) => {
-  const performAction = (
-    action:
-      | "MOVE_FORWARD"
-      | "MOVE_BACKWARD"
-      | "STEP_LEFT"
-      | "STEP_RIGHT"
-      | "TURN_LEFT"
-      | "TURN_RIGHT"
-      | "STAND"
-      | "REST",
-    repeat = Infinity
-  ) =>
+  const performAction = (action: "STAND" | "REST") =>
     sendCommand({
       command: "PERFORM_ACTIONS",
       args: {
@@ -26,43 +15,26 @@ export const MovementControl = ({
           {
             type: action,
             args: {},
-            repeat,
+            repeat: 1,
           },
         ],
       },
-    });
-  const stop = () =>
-    sendCommand({
-      command: "STOP_ACTIONS",
-      args: {},
     });
 
   return (
     <div>
       <h2 className="text-4xl font-bold dark:text-white">Movement control</h2>
       <div className="flex gap-1">
-        <ActionButton action={() => performAction("MOVE_FORWARD")} stop={stop}>
-          Forward
-        </ActionButton>
-        <ActionButton action={() => performAction("MOVE_BACKWARD")} stop={stop}>
-          Backward
-        </ActionButton>
-        <ActionButton action={() => performAction("STEP_LEFT")} stop={stop}>
-          Step left
-        </ActionButton>
-        <ActionButton action={() => performAction("STEP_RIGHT")} stop={stop}>
-          Step right
-        </ActionButton>
-        <ActionButton action={() => performAction("TURN_LEFT")} stop={stop}>
-          Turn left
-        </ActionButton>
-        <ActionButton action={() => performAction("TURN_RIGHT")} stop={stop}>
-          Turn right
-        </ActionButton>
+        <ActionButton action="MOVE_FORWARD">Forward</ActionButton>
+        <ActionButton action="MOVE_BACKWARD">Backward</ActionButton>
+        <ActionButton action="STEP_LEFT">Step left</ActionButton>
+        <ActionButton action="STEP_RIGHT">Step right</ActionButton>
+        <ActionButton action="TURN_LEFT">Turn left</ActionButton>
+        <ActionButton action="TURN_RIGHT">Turn right</ActionButton>
       </div>
       <div className="flex gap-1">
-        <Button onClick={() => performAction("STAND", 1)}>Stand</Button>
-        <Button onClick={() => performAction("REST", 1)}>Rest</Button>
+        <Button onClick={() => performAction("STAND")}>Stand</Button>
+        <Button onClick={() => performAction("REST")}>Rest</Button>
       </div>
     </div>
   );
--- web-ui/src/components/ui/action-button.tsx
+++ web-ui/src/components/ui/action-button.tsx
@@ -1,30 +1,65 @@
 import { ReactNode, useRef } from "react";
 import { Button } from "./button";
+import { useRobotServer } from "@/hooks/use-robot-server";
+
+const DELAY_TIME = 500;
 
 export const ActionButton = ({
   action,
-  stop,
   children,
   className,
 }: {
-  action: () => void;
-  stop: () => void;
+  action:
+    | "MOVE_FORWARD"
+    | "MOVE_BACKWARD"
+    | "STEP_LEFT"
+    | "STEP_RIGHT"
+    | "TURN_LEFT"
+    | "TURN_RIGHT";
   children: ReactNode;
   className?: string;
 }) => {
+  const { sendCommand } = useRobotServer();
+
+  const mouseDownTimeRef = useRef<number>(0);
   const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const performAction = (repeat: number) =>
+    sendCommand({
+      command: "PERFORM_ACTIONS",
+      args: {
+        actions: [
+          {
+            type: action,
+            args: {},
+            repeat,
+          },
+        ],
+      },
+    });
   const continueAction = () => {
-    action();
+    performAction(Infinity);
     timerRef.current = setTimeout(continueAction, 1000);
   };
+
+  const stop = () =>
+    sendCommand({
+      command: "STOP_ACTIONS",
+      args: {},
+    });
 
   return (
     <Button
       className={className}
-      onMouseDown={continueAction}
+      onMouseDown={() => {
+        mouseDownTimeRef.current = Date.now();
+        performAction(1);
+        timerRef.current = setTimeout(continueAction, DELAY_TIME);
+      }}
       onMouseUp={() => {
         timerRef.current && clearTimeout(timerRef.current);
-        stop();
+        if (Date.now() - mouseDownTimeRef.current > DELAY_TIME) {
+          stop();
+        }
       }}
     >
       {children}
